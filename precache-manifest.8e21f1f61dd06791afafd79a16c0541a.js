070386ce86fcb686a780c91b29a25f1826e5f188
Lucien <lygm1062@gmail.com>
2018-11-13 15:33:31 +0800

    6d4925f master

--- precache-manifest.8e21f1f61dd06791afafd79a16c0541a.js
+++ precache-manifest.8e21f1f61dd06791afafd79a16c0541a.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/mahwyst/static/js/runtime~main.6a01d1a4.js"
   },
   {
-    "revision": "bc8c2f3675017e433d7b",
-    "url": "/mahwyst/static/js/main.bc8c2f36.chunk.js"
+    "revision": "8df0950707c71dec62cb",
+    "url": "/mahwyst/static/js/main.8df09507.chunk.js"
   },
   {
     "revision": "3acc18631ab83b0bdfd5",
     "url": "/mahwyst/static/js/1.3acc1863.chunk.js"
   },
   {
-    "revision": "bc8c2f3675017e433d7b",
+    "revision": "8df0950707c71dec62cb",
     "url": "/mahwyst/static/css/main.ac6f1ca9.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/mahwyst/static/css/1.f3fb3827.chunk.css"
   },
   {
-    "revision": "84427f4478be564f03b3da8dfcac0daf",
+    "revision": "b8e82fb60a5ddf0c5f059c5d3ddd41df",
     "url": "/mahwyst/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/mahwyst/precache-manifest.571ab314ad6713d8d164b84cd61ea04b.js"
+  "/mahwyst/precache-manifest.8e21f1f61dd06791afafd79a16c0541a.js"
 );
 
 workbox.clientsClaim();
